1b5f177226d04d2744c7e184851fae475562b1be
NadezhdaSakal <sakaln88@gmail.com>
2025-02-10 17:45:59 +0300

    add animation with JC

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,18 +19,18 @@ fun MainScreen() {
     Scaffold { paddingValues ->
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues), contentAlignment = Alignment.Center
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
 
             CustomContainerCompose(
                 firstChild = {
-                    // TODO
-                    // ...
+                    Text("First Child", Modifier.padding(paddingValues))
+
                 },
                 secondChild = {
-                    // TODO
-                    // ...
+                    Text("Second Child", Modifier.padding(paddingValues))
+
                 }
             )
         }
