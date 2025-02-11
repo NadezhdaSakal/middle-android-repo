69b8b94a1f28aa9b274adbc1cacf1352e91dade7
NadezhdaSakal <sakaln88@gmail.com>
2025-02-11 22:40:40 +0300

    replace Layout with Box

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
@@ -2,23 +2,12 @@ package com.example.androidpracticumcustomview.ui.theme
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.Layout
 import kotlinx.coroutines.launch
-
-/*
-Задание:
-Реализуйте необходимые компоненты;
-Создайте проверку что дочерних элементов не более 2-х;
-Предусмотрите обработку ошибок рендера дочерних элементов.
-Задание по желанию:
-Предусмотрите параметризацию длительности анимации.
- */
 
 private const val ALPHA_DURATION = 2000
 private const val MOVEMENT_DURATION = 5000
@@ -26,17 +15,21 @@ private const val MAX_CHILD_COUNT = 2
 
 @Composable
 fun CustomContainerCompose(
-    firstChild: @Composable (() -> Unit),
-    secondChild: @Composable (() -> Unit)
+    firstChild: @Composable (() -> Unit)? = null,
+    secondChild: @Composable (() -> Unit)? = null
 ) {
+    require(listOfNotNull(firstChild, secondChild).size <= MAX_CHILD_COUNT) {
+        "Невозможно добавить более $MAX_CHILD_COUNT дочерних элементов в этот контейнер"
+    }
+
     val offsetY = remember { Animatable(0f) }
     val alpha = remember { Animatable(0f) }
-    val screenHeight = remember { mutableFloatStateOf(0f) }
+    var screenHeight by remember { mutableStateOf(0f) }
 
     LaunchedEffect(Unit) {
         launch {
             offsetY.animateTo(
-                targetValue = screenHeight.floatValue / 2,
+                targetValue = screenHeight / 2,
                 animationSpec = tween(MOVEMENT_DURATION)
             )
         }
@@ -48,32 +41,26 @@ fun CustomContainerCompose(
         }
     }
 
-    Layout(
-        modifier = Modifier.alpha(alpha.value),
-        content = {
-            firstChild()
-            secondChild()
-        }
-    ) { measurableList, constraints ->
-        val placeableList = measurableList.map { measurable ->
-            measurable.measure(constraints)
-        }
+    Box(modifier = Modifier.alpha(alpha.value)) {
+        Layout(
+            content = {
+                firstChild?.let { Box(Modifier) { it() } }
+                secondChild?.let { Box(Modifier) { it() } }
+            }
+        ) { measurableList, constraints ->
+            screenHeight = constraints.maxHeight.toFloat() - 100
 
-        if (placeableList.size > MAX_CHILD_COUNT) {
-            throw IllegalStateException(
-                "Невозможно добавить более $MAX_CHILD_COUNT дочерних элементов в этот контейнер"
-            )
-        }
+            val placeableList = measurableList.map { measurable ->
+                measurable.measure(constraints)
+            }
 
-        layout(constraints.maxWidth, constraints.maxHeight) {
-            placeableList.forEachIndexed { index, placeable ->
-                screenHeight.floatValue = constraints.maxHeight.toFloat() - 100
-                val x = constraints.maxWidth / 2 - placeable.width / 2
-                val y = constraints.maxHeight / 2
-                if (index == 0) {
-                    placeable.placeRelative(x = x, y = (y - offsetY.value.toInt()))
-                } else {
-                    placeable.placeRelative(x = x, y = (offsetY.value.toInt() + y))
+            layout(constraints.maxWidth, constraints.maxHeight) {
+                placeableList.forEachIndexed { index, placeable ->
+                    val x = constraints.maxWidth / 2 - placeable.width / 2
+                    val y = constraints.maxHeight / 2
+                    val yOffset = if (index == 0) -offsetY.value.toInt() else offsetY.value.toInt()
+
+                    placeable.placeRelative(x = x, y = y + yOffset)
                 }
             }
         }
